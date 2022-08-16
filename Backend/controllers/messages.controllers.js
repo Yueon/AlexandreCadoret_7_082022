4831670012d0f26dd4ec9689e59c8653164ec7ff
Yueon <cado5alexandre@gmail.com>
2022-08-16 15:41:22 +0200

    CRUD comments

--- Backend/controllers/messages.controllers.js
+++ Backend/controllers/messages.controllers.js
@@ -6,7 +6,7 @@ module.exports.readPost = (req, res) => {
     postModel.find((err, docs) => {
         if (!err) res.send(docs);
         else console.log('Error to get data : ' + err);
-    })
+    }).sort({ createdAt: -1 });
 };
 
 module.exports.createPost = async (req, res) => {
@@ -138,4 +138,82 @@ module.exports.like = (req, res, next) => {
         })
         // si erreur envoit un status 404 Not Found et l'erreur en json
         .catch((error) => res.status(404).json({ error }));
+};
+
+///////////////////////////Comments/////////////////////////////
+
+module.exports.commentPost = (req, res) => {
+    if (!objectId.isValid(req.params.id))
+        return res.status(400).send('ID inconnu : ' + req.params.id);
+
+    try {
+        return PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $push: {
+                    comments: {
+                        commenterId: req.body.commenterId,
+                        commenterPseudo: req.body.commenterPseudo,
+                        text: req.body.text,
+                        timestamp: new Date().getTime(),
+                    },
+                },
+            },
+            { new: true },
+            (err, docs) => {
+                if (!err) return res.send(docs);
+                else return res.status(400).send(err);
+            }
+        );
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+};
+
+module.exports.editCommentPost = (req, res) => {
+    if (!objectId.isValid(req.params.id))
+        return res.status(400).send('ID inconnu : ' + req.params.id);
+
+    try {
+        return PostModel.findById(req.params.id, (err, docs) => {
+            const theComment = docs.comments.find((comment) =>
+                comment._id.equals(req.body.commentId)
+            );
+
+            if (!theComment) return res.status(404).send("Comment not found");
+            theComment.text = req.body.text;
+
+            return docs.save((err) => {
+                if (!err) return res.status(200).send(docs);
+                return res.status(500).send(err);
+            });
+        });
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+};
+
+module.exports.deleteCommentPost = (req, res) => {
+    if (!objectId.isValid(req.params.id))
+        return res.status(400).send('ID inconnu : ' + req.params.id);
+
+    try {
+        return PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $pull: {
+                    comments: {
+                        _id: req.body.commentId,
+                    },
+                },
+            },
+            { new: true },
+            (err, docs) => {
+                if (!err) return res.send(docs);
+                else return res.status(400).send(err);
+            }
+        );
+    } catch (err) {
+        return res.status(400).send(err);
+    }
 };
--- Backend/routes/messages.routes.js
+++ Backend/routes/messages.routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 const postController = require('../controllers/messages.controllers');
 
 router.get('/', auth, postController.readPost);
-router.post('/', auth, postController.createPost);
-router.put('/:id', auth, postController.updatePost);
+router.post('/', auth, multer, postController.createPost);
+router.put('/:id', auth, multer, postController.updatePost);
 router.delete('/:id', auth, postController.deletePost);
 router.post("/:id/like", auth, postController.like)
+
+//comments
+router.patch('/comment-post/:id', postController.commentPost);
+router.patch('/edit-comment-post/:id', postController.editCommentPost);
+router.patch('/delete-comment-post/:id', postController.deleteCommentPost);
+
 
 module.exports = router;
