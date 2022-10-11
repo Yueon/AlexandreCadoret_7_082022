fe886c9df537f2cbf221cc75441da27e90859e93
Yueon <cado5alexandre@gmail.com>
2022-10-11 16:41:10 +0200

    message

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -131,6 +131,45 @@ module.exports.updateUser = async (req, res) => {
     }
 };
 
+module.exports.uploadProfil = async (req, res) => {
+    try {
+        if (
+            req.file.detectedMimeType != "image/jpg" &&
+            req.file.detectedMimeType != "image/png" &&
+            req.file.detectedMimeType != "image/jpeg"
+        )
+            throw Error("invalid file");
+
+        if (req.file.size > 500000) throw Error("max size");
+    } catch (err) {
+        const errors = uploadErrors(err);
+        return res.status(201).json({ errors });
+    }
+    const fileName = req.body.name + ".jpg";
+
+    await pipeline(
+        req.file.stream,
+        fs.createWriteStream(
+            `${__dirname}../images/${fileName}`
+        )
+    );
+
+    try {
+        await userModel.findByIdAndUpdate(
+            req.body.userId,
+            { $set: { image: "../images/" + fileName } },
+            { new: true, upsert: true, setDefaultsOnInsert: true },
+            (err, docs) => {
+                if (!err) return res.send(docs);
+                else return res.status(500).send({ message: err });
+            }
+        );
+    } catch (err) {
+        return res.status(500).send({ message: err });
+    }
+};
+
+
 module.exports.deleteUser = async (req, res) => {
     if (!ObjectID.isValid(req.params.id))
         return res.status(400).send('ID inconnu : ' + req.params.id)
